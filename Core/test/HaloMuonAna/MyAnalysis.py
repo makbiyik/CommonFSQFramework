ebea00ecc3429529a14ed33cc35a2821934c8686
makbiyik <melike.akbiyik@kit.edu>
2016-02-04 12:17:03 +0100

    up-to-date

--- Core/test/HaloMuonAna/MyAnalysis.py
+++ Core/test/HaloMuonAna/MyAnalysis.py
@@ -4,13 +4,16 @@ PATbasePATH="/pnfs/desy/cms/"
 # TTreeBasePATH="root://eoscms.cern.ch//eos/cms/"
 # ROOTPrefix="root://xrootd.unl.edu/" # USA
 # ROOTPrefix=""
-#ROOTPrefix="root://xrootd-cms.infn.it" # Europe+Asia
+# ROOTPrefix="root://xrootd-cms.infn.it" # Europe+Asia
 ROOTPrefix="root://cms-xrd-global.cern.ch/" # Global
 
 # for EOS:
-#PATbasePATH=""
-#TTreeBasePATH=""
-#ROOTPrefix="root://eoscms//eos/cms/"
+# PATbasePATH=""
+# TTreeBasePATH=""
+# ROOTPrefix="root://eoscms//eos/cms/"
+# ROOTPrefix=""
+# TTreeBasePATH="root://eoscms.cern.ch//eos/cms/"
+# PATbasePATH="root://eoscms.cern.ch//eos/cms/"
 
 #for local:
 # PATbasePATH="/afs/cern.ch/work/m/makbiyik/public/"
